f748306970d572f5dd2762398a5a3ec20ab91111
Maikon Igor <maikonigor@gmail.com>
2023-06-02 13:57:24 -0300

    replace backbutton link

--- components/back-button/back-button.jsx
+++ components/back-button/back-button.jsx
@@ -3,11 +3,12 @@ import styles from './backButton.module.css'
 import Image from 'next/image'
 export function BackButton({ url, text }) {
     return (
-        <div className={styles.backLink}>
-            <Link href={url}>
-                <Image src="/icons/arrow-left.svg" width={12} height={12}></Image>
+
+        <Link href={url}>
+            <div className={styles.backLink}>
+                <Image className={styles.backIcon} src="/icons/arrow-left.svg" width={12} height={12}></Image>
                 <span className='margin-left-1'>{text}</span>
-            </Link>
-        </div>
+            </div>
+        </Link>
     )
 }
